abbc4fabe6446cee2e359159a5b964ab8c8114b8
kondrei <kondrei@gmail.com>
2020-12-02 13:47:34 +0200

    last commit of second day of exercises

--- 02/script.js
+++ 02/script.js
@@ -34,10 +34,111 @@ console.log(string.lastIndexOf('a'))
 //Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 console.log('You cannot end a sentence with because because because is a conjunction'.indexOf('because'))
 //Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf('because'))
 //Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log('You cannot end a sentence with because because because is a conjunction'.search('because'))
 //Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+console.log(' 30 Days Of JavaScript '.trim())
 //Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(string.startsWith(30))
 //Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(string.endsWith('Script'))
 //Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(string.match(/a+/gi))
 //Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+console.log(string.concat('30 Days of','JavaScript'))
 //Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(string.repeat(2))
+
+
+
+//Exercise: Level 2
+//Using console.log() print out the following statement:
+//
+//The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+//Using console.log() print out the following quote by Mother Teresa:
+//
+
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+//
+console.log(typeof(10) == 'number');
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+//
+console.log(Math.round(parseFloat('9.8')) == 10)
+
+//Check if 'on' is found in both python and jargon
+//
+console.log('python'.match(/on+/gi))
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+//
+//Generate a random number between 0 and 100 inclusively.
+//
+//Generate a random number between 50 and 100 inclusively.
+//
+//Generate a random number between 0 and 255 inclusively.
+//
+//Access the 'JavaScript' string characters using a random number.
+//
+//Use console.log() and escape characters to print the following pattern.
+//
+//1 1 1 1 1
+//2 1 2 4 8
+//3 1 3 9 27
+//4 1 4 16 64
+//5 1 5 25 125
+//Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+//
+//Exercises: Level 3
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+//
+//Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+//
+//Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+//
+    //
+//const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+//Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+//Exercise: Level 2
+//Using console.log() print out the following statement:
+//
+//The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+//Using console.log() print out the following quote by Mother Teresa:
+//
+//"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+//
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+//
+//Check if 'on' is found in both python and jargon
+//
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+//
+//Generate a random number between 0 and 100 inclusively.
+//
+//Generate a random number between 50 and 100 inclusively.
+//
+//Generate a random number between 0 and 255 inclusively.
+//
+//Access the 'JavaScript' string characters using a random number.
+//
+//Use console.log() and escape characters to print the following pattern.
+//
+//1 1 1 1 1
+//2 1 2 4 8
+//3 1 3 9 27
+//4 1 4 16 64
+//5 1 5 25 125
+//Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+//
+//Exercises: Level 3
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+//
+//Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+//
+//Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+//
+    //
+//const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+//Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
