cee1f1181bb5a0740586b3a9f50906106c7e0004
Dan Andrei <kondrei@gmail.com>
2021-02-26 15:22:02 +0200

    test ipad

--- 05/arrays.js
+++ 05/arrays.js
@@ -136,6 +136,8 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 console.log(ages.sort());
 console.log(Math.max(...ages))
 // Sort the array and find the min and max age
+ages.sort()
+
 // Find the median age(one middle item or two middle items divided by two)
 // Find the average age(all items divided by number of items)
 // Find the range of the ages(max minus min)
